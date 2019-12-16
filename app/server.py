13ab8e676b93a4a583868b700343f07277701e46
Tyler Noblett <Tyler.Noblett@outlook.com>
2019-12-16 18:58:22 -0500

    add log

--- app/server.py
+++ app/server.py
@@ -58,6 +58,7 @@ async def homepage(request):
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
     img_data = await request.form()
+    await print(img_data)
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
