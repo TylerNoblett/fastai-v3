9b8b8ac7b64802149022693c7d0f13fe5d8f7dd6
Tyler Noblett <Tyler.Noblett@outlook.com>
2019-12-16 19:09:55 -0500

    corrected

--- app/server.py
+++ app/server.py
@@ -57,9 +57,9 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
+    print('request', request)
     img_data = await request.form()
-    await print('request', request)
-    await print('img_data', img_data)
+    print('img_data', img_data)
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
