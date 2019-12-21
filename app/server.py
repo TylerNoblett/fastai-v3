6ac9717f9cb9fcce369f2decaf01622384a4f0a1
Tyler Noblett <Tyler.Noblett@outlook.com>
2019-12-21 17:41:31 -0500

    remove unnecessary await

--- app/server.py
+++ app/server.py
@@ -58,8 +58,7 @@ async def homepage(request):
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
     img_data = await request.form()
-    img_str = await (img_data['file'])
-    imgdata = base64.b64decode(img_str)
+    imgdata = base64.b64decode(img_data['file'])
     filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
     with open(filename, 'wb') as f:
         f.write(imgdata)    
