9367ba2c92cbd247a750ba320f02c78a4ce34770
Tyler Noblett <Tyler.Noblett@outlook.com>
2019-12-21 17:26:20 -0500

    potential transformation of base64 to img

--- app/server.py
+++ app/server.py
@@ -57,13 +57,17 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    print('request', request)
     img_data = await request.form()
-    print('img_data', img_data)
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
+    img_str = await (img_data['file'])
+    imgdata = base64.b64decode(img_str)
+    filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+    with open(filename, 'wb') as f:
+        f.write(imgdata)    
+    img = open_image(filename)
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    result = f'result: ${str(prediction)}'
+    print(result)
+    return JSONResponse({result})
 
 
 if __name__ == '__main__':
